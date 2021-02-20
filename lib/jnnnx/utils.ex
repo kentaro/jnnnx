e2a4145a1eeed254bcb873e945e0a4e242ec627c
Kentaro Kuribayashi <kentarok@gmail.com>
2021-02-20 23:30:03 +0900

    add softmax function

--- lib/jnnnx/utils.ex
+++ lib/jnnnx/utils.ex
@@ -1,11 +1,13 @@
 defmodule Jnnnx.Utils do
+  import Nx.Defn
+
   @doc """
   Converts a class vector to a matrix of one-hot-encoded vectors.
 
   ## Examples
 
   ```
-  iex(2)> y_train = Jnnnx.Utils.to_categorical(y_train, 10)
+  iex> Jnnnx.Utils.to_categorical(y_train, 10)
   #Nx.Tensor<
     u8[60000][10]
     [
@@ -27,5 +29,28 @@ defmodule Jnnnx.Utils do
     t
     |> Nx.reshape({shape, 1})
     |> Nx.equal(o)
+  end
+
+  @doc """
+  Implements the softmax function.
+
+  ## Examples
+
+  ```
+  iex> t = Nx.tensor([1,2,3,4])
+  #Nx.Tensor<
+    s64[4]
+    [1, 2, 3, 4]
+  >
+  iex> Jnnnx.Utils.softmax(t)
+  #Nx.Tensor<
+    f64[4]
+    [0.03205860328008499, 0.08714431874203257, 0.23688281808991013, 0.6439142598879722]
+  >
+  ```
+
+  """
+  defn softmax(t) do
+    Nx.exp(t) / Nx.sum(Nx.exp(t))
   end
 end
