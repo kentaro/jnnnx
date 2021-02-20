fb92ed13ae78412da7e10f738bc7f24135a687a5
Kentaro Kuribayashi <kentarok@gmail.com>
2021-02-20 20:50:28 +0900

    mix new jnnnx

---
+++ lib/jnnnx.ex
@@ -0,0 +1,18 @@
+defmodule Jnnnx do
+  @moduledoc """
+  Documentation for `Jnnnx`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Jnnnx.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/jnnnx_test.exs
@@ -0,0 +1,8 @@
+defmodule JnnnxTest do
+  use ExUnit.Case
+  doctest Jnnnx
+
+  test "greets the world" do
+    assert Jnnnx.hello() == :world
+  end
+end
